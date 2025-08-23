bdf4ef217e5bc4213623109ded9c6aafa1f43826
methlygupta23 <methlygupta7@gmail.com>
2025-08-23 13:44:41 +0530

    made polcies page

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,50 @@ const mysql = require('mysql2');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
+const bycrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const razorpay = require('razorpay');
+const nodeMailer = require('nodemailer');
+const crypto = require('crypto');
+const formidable = require('formidable');
+const fs = require('fs');
+const rateLimit = require('express-rate-limit');
+
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const app = express();
+
+// Middleware
+app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 
+// Database connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user:'root',
-    password: 'Password',
-    database: 'nexsphere',
+    host: process.env.DB_HOST ,
+    user:process.env.DB_USER ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME,
 });
 
+//Razorpay instance
+const razorpay = new Razorpay ({
+    key_id : process.env.RAZORPAY_KEY_ID,
+    key_secret : process.env.RAZORPAY_KEY_SECRET,
+});
+
+// Email transporter setup
+const transporter = nodeMailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+});
+
+// Connect to the database
 db.connect((err)=>{
     if(err){
         console.log('Error connecting to the Mysql:', err);
